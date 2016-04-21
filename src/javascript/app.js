c368c20028419c325739ce0b9d7dd935e9d9dc6d
James Gosbell <james.gosbell@servicerocket.com>
2016-04-22 01:51:12 +1000

    Updated portfolio with banner image and updated js

--- src/javascript/app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(".c-hamburger").click(function() {
-        $(this).stop().toggleClass("is-active");
-    });
-    $('.toggle-nav').click(function() {
-        $('body').toggleClass('show-nav');
-        return false;
-    });
-})();
